480c46364740e3589f7297b62e27fc57fc941a6f
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-12-26 22:31:37 +1100

    Make script external

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// import * as data from './data.json';
-
-window.onload = function () {
-  console.log("Hello world!");
-  setBarHeights();
-};
-
-console.log("Hello world!");
 let bars = document.getElementsByClassName("bar");
-for (let i = 0; i < bars.length; i++) {
-  bars[i].style.height = `${data[i].amount}px`;
-}
+fetch("./data.json")
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < bars.length; i++) {
+      bars[i].style.height = `${data[i].amount * 3}px`;
+    }
+  });
 
 // function show prices on hover
 
